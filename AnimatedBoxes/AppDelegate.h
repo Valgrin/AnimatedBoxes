ddc14e27be0309715caf661f7eac275fba2c7a5e
Thomas LEVY <thomas.levy@mappy.com>
2015-01-28 09:08:19 +0100

    Initialize xcode project

---
+++ AnimatedBoxes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatedBoxes
+//
+//  Created by Thomas LEVY on 28/01/15.
+//  Copyright (c) 2015 Thomas LEVY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatedBoxes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatedBoxes
+//
+//  Created by Thomas LEVY on 28/01/15.
+//  Copyright (c) 2015 Thomas LEVY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
